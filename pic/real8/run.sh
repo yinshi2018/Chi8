14e4d05319bf135f4d7cc45879b5769be6600dba
mac <mac@macdeMacBook-Air-2.local>
2020-03-15 17:05:32 +0800

    pic

---
+++ pic/real8/run.sh
@@ -0,0 +1,187 @@
+cp -u ./mub0/deltamu250/exam/PolyakovEq.f90 ./mub20/deltamu250/exam
+cp -u ./mub0/deltamu250/exam/selfEQ.f90 ./mub20/deltamu250/exam
+
+cp -u ./mub0/deltamu250/exam/PolyakovEq.f90 ./mub40/deltamu250/exam
+cp -u ./mub0/deltamu250/exam/selfEQ.f90 ./mub40/deltamu250/exam
+
+cp -u ./mub0/deltamu250/exam/PolyakovEq.f90 ./mub60/deltamu250/exam
+cp -u ./mub0/deltamu250/exam/selfEQ.f90 ./mub60/deltamu250/exam
+
+cp -u ./mub0/deltamu250/exam/PolyakovEq.f90 ./mub80/deltamu250/exam
+cp -u ./mub0/deltamu250/exam/selfEQ.f90 ./mub80/deltamu250/exam
+
+cp -u ./mub0/deltamu250/exam/PolyakovEq.f90 ./mub100/deltamu250/exam
+cp -u ./mub0/deltamu250/exam/selfEQ.f90 ./mub100/deltamu250/exam
+
+cp -u ./mub0/deltamu250/exam/PolyakovEq.f90 ./mub120/deltamu250/exam
+cp -u ./mub0/deltamu250/exam/selfEQ.f90 ./mub120/deltamu250/exam
+
+cp -u ./mub0/deltamu250/exam/PolyakovEq.f90 ./mub140/deltamu220/exam
+cp -u ./mub0/deltamu250/exam/selfEQ.f90 ./mub140/deltamu220/exam
+
+cp -u ./mub0/deltamu250/exam/PolyakovEq.f90 ./mub160/deltamu220/exam
+cp -u ./mub0/deltamu250/exam/selfEQ.f90 ./mub160/deltamu220/exam
+
+cp -u ./mub0/deltamu250/exam/PolyakovEq.f90 ./mub180/deltamu210/exam
+cp -u ./mub0/deltamu250/exam/selfEQ.f90 ./mub180/deltamu210/exam
+
+cp -u ./mub0/deltamu250/exam/PolyakovEq.f90 ./mub200/deltamu200/exam
+cp -u ./mub0/deltamu250/exam/selfEQ.f90 ./mub200/deltamu200/exam
+
+cp -u ./mub0/deltamu250/exam/PolyakovEq.f90 ./mub220/deltamu210/exam
+cp -u ./mub0/deltamu250/exam/selfEQ.f90 ./mub220/deltamu210/exam
+
+cp -u ./mub0/deltamu250/exam/PolyakovEq.f90 ./mub240/deltamu200/exam
+cp -u ./mub0/deltamu250/exam/selfEQ.f90 ./mub240/deltamu200/exam
+
+cp -u ./mub0/deltamu250/exam/PolyakovEq.f90 ./mub260/deltamu200/exam
+cp -u ./mub0/deltamu250/exam/selfEQ.f90 ./mub260/deltamu200/exam
+
+cp -u ./mub0/deltamu250/exam/PolyakovEq.f90 ./mub280/deltamu200/exam
+cp -u ./mub0/deltamu250/exam/selfEQ.f90 ./mub280/deltamu200/exam
+
+cp -u ./mub0/deltamu250/exam/PolyakovEq.f90 ./mub300/deltamu190/exam
+cp -u ./mub0/deltamu250/exam/selfEQ.f90 ./mub300/deltamu190/exam
+
+cp -u ./mub0/deltamu250/exam/PolyakovEq.f90 ./mub320/deltamu185/exam
+cp -u ./mub0/deltamu250/exam/selfEQ.f90 ./mub320/deltamu185/exam
+
+cp -u ./mub0/deltamu250/exam/PolyakovEq.f90 ./mub340/deltamu180/exam
+cp -u ./mub0/deltamu250/exam/selfEQ.f90 ./mub340/deltamu180/exam
+
+cp -u ./mub0/deltamu250/exam/PolyakovEq.f90 ./mub360/deltamu175/exam
+cp -u ./mub0/deltamu250/exam/selfEQ.f90 ./mub360/deltamu175/exam
+
+cp -u ./mub0/deltamu250/exam/PolyakovEq.f90 ./mub380/deltamu170/exam
+cp -u ./mub0/deltamu250/exam/selfEQ.f90 ./mub380/deltamu170/exam
+
+cp -u ./mub0/deltamu250/exam/PolyakovEq.f90 ./mub400/deltamu160/exam
+cp -u ./mub0/deltamu250/exam/selfEQ.f90 ./mub400/deltamu160/exam
+
+cd mub0 
+cd deltamu250
+./run.sh
+cp -r -u ./final ..
+cd ..
+cd ..
+cd mub20 
+cd deltamu250
+./run.sh
+cp -r -u ./final ..
+cd ..
+cd ..
+cd mub40 
+cd deltamu250
+./run.sh
+cp -r -u ./final ..
+cd ..
+cd ..
+cd mub60 
+cd deltamu250
+./run.sh
+cp -r -u ./final ..
+cd ..
+cd ..
+cd mub80 
+cd deltamu250
+./run.sh
+cp -r -u ./final ..
+cd ..
+cd ..
+cd mub100 
+cd deltamu250
+./run.sh
+cp -r -u ./final ..
+cd ..
+cd ..
+cd mub120 
+cd deltamu250
+./run.sh
+cp -r -u ./final ..
+cd ..
+cd ..
+cd mub140 
+cd deltamu220
+./run.sh
+cp -r -u ./final ..
+cd ..
+cd ..
+cd mub160 
+cd deltamu220
+./run.sh
+cp -r -u ./final ..
+cd ..
+cd ..
+cd mub180 
+cd deltamu210
+./run.sh
+cp -r -u ./final ..
+cd ..
+cd ..
+cd mub200 
+cd deltamu200
+./run.sh
+cp -r -u ./final ..
+cd ..
+cd ..
+cd mub220 
+cd deltamu210
+./run.sh
+cp -r -u ./final ..
+cd ..
+cd ..
+cd mub240 
+cd deltamu200
+./run.sh
+cp -r -u ./final ..
+cd ..
+cd ..
+cd mub260 
+cd deltamu200
+./run.sh
+cp -r -u ./final ..
+cd ..
+cd ..
+cd mub280 
+cd deltamu200
+./run.sh
+cp -r -u ./final ..
+cd ..
+cd ..
+cd mub300 
+cd deltamu190
+./run.sh
+cp -r -u ./final ..
+cd ..
+cd ..
+cd mub320 
+cd deltamu185
+./run.sh
+cp -r -u ./final ..
+cd ..
+cd ..
+cd mub340 
+cd deltamu180
+./run.sh
+cp -r -u ./final ..
+cd ..
+cd ..
+cd mub360 
+cd deltamu175
+./run.sh
+cp -r -u ./final ..
+cd ..
+cd ..
+cd mub380 
+cd deltamu170
+./run.sh
+cp -r -u ./final ..
+cd ..
+cd ..
+cd mub400 
+cd deltamu160
+./run.sh
+cp -r -u ./final ..
+cd ..
+cd ..
+python3 Rmu.py
