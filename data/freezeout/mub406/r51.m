b346499c53a7953d489438af039e93f068748fe8
yinshi2018 <yinshi2014@mail.dlut.edu.cn>
2020-11-10 20:22:09 +0800

    R51

---
+++ data/freezeout/mub406/r51.m
@@ -0,0 +1,86 @@
+cd cmucen
+cd final
+cd buffer
+load('chi5.dat');
+load('chi1.dat');
+r51cen=chi5./chi1;
+cd ..
+cd ..
+cd ../cmudown
+cd final
+cd buffer
+load('chi5.dat');
+load('chi1.dat');
+r51down=chi5./chi1;
+cd ..
+cd ..
+cd ../cmudown1
+cd final
+cd buffer
+load('chi5.dat');
+load('chi1.dat');
+r51down1=chi5./chi1;
+cd ..
+cd ..
+cd ../cmudown1
+cd final
+cd buffer
+load('chi5.dat');
+load('chi1.dat');
+r51down1=chi5./chi1;
+cd ..
+cd ..
+cd ../cmudown3
+cd final
+cd buffer
+load('chi5.dat');
+load('chi1.dat');
+r51down3=chi5./chi1;
+cd ..
+cd ..
+cd ../cmudown4
+cd final
+cd buffer
+load('chi5.dat');
+load('chi1.dat');
+r51down4=chi5./chi1;
+cd ..
+cd ..
+cd ../cmuup
+cd final
+cd buffer
+load('chi5.dat');
+load('chi1.dat');
+r51up=chi5./chi1;
+cd ..
+cd ..
+cd ../cmuup1
+cd final
+cd buffer
+load('chi5.dat');
+load('chi1.dat');
+r51up1=chi5./chi1;
+cd ..
+cd ..
+cd ../cmuup1
+cd final
+cd buffer
+load('chi5.dat');
+load('chi1.dat');
+r51up1=chi5./chi1;
+cd ..
+cd ..
+cd ../cmuup3
+cd final
+cd buffer
+load('chi5.dat');
+load('chi1.dat');
+r51up3=chi5./chi1;
+cd ..
+cd ..
+cd ../cmuup4
+cd final
+cd buffer
+load('chi5.dat');
+load('chi1.dat');
+r51up4=chi5./chi1;
---
+++ data/freezeout/mub406/r52.m
@@ -0,0 +1,86 @@
+cd cmucen
+cd final
+cd buffer
+load('chi5.dat');
+load('chi2.dat');
+r52cen=chi5./chi2;
+cd ..
+cd ..
+cd ../cmudown
+cd final
+cd buffer
+load('chi5.dat');
+load('chi2.dat');
+r52down=chi5./chi2;
+cd ..
+cd ..
+cd ../cmudown1
+cd final
+cd buffer
+load('chi5.dat');
+load('chi2.dat');
+r52down1=chi5./chi2;
+cd ..
+cd ..
+cd ../cmudown2
+cd final
+cd buffer
+load('chi5.dat');
+load('chi2.dat');
+r52down2=chi5./chi2;
+cd ..
+cd ..
+cd ../cmudown3
+cd final
+cd buffer
+load('chi5.dat');
+load('chi2.dat');
+r52down3=chi5./chi2;
+cd ..
+cd ..
+cd ../cmudown4
+cd final
+cd buffer
+load('chi5.dat');
+load('chi2.dat');
+r52down4=chi5./chi2;
+cd ..
+cd ..
+cd ../cmuup
+cd final
+cd buffer
+load('chi5.dat');
+load('chi2.dat');
+r52up=chi5./chi2;
+cd ..
+cd ..
+cd ../cmuup1
+cd final
+cd buffer
+load('chi5.dat');
+load('chi2.dat');
+r52up1=chi5./chi2;
+cd ..
+cd ..
+cd ../cmuup2
+cd final
+cd buffer
+load('chi5.dat');
+load('chi2.dat');
+r52up2=chi5./chi2;
+cd ..
+cd ..
+cd ../cmuup3
+cd final
+cd buffer
+load('chi5.dat');
+load('chi2.dat');
+r52up3=chi5./chi2;
+cd ..
+cd ..
+cd ../cmuup4
+cd final
+cd buffer
+load('chi5.dat');
+load('chi2.dat');
+r52up4=chi5./chi2;
